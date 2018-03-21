20e465376f114aa8b490f3a88ea367be0a6103db
Haroon Alam <syedharoonalam@gmail.com>
2018-03-21 16:07:17 +0000

    For L4T 28.2

--- scripts/getKernelSources.sh
+++ scripts/getKernelSources.sh
@@ -3,9 +3,9 @@ apt-add-repository universe
 apt-get update
 apt-get install qt5-default pkg-config -y
 cd /usr/src
-wget http://developer.download.nvidia.com/embedded/L4T/r28_Release_v1.0/BSP/source_release.tbz2
-tar -xvf source_release.tbz2 sources/kernel_src-tx2.tbz2
-tar -xvf sources/kernel_src-tx2.tbz2
+wget http://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.0/BSP/source_release.tbz2
+tar -xvf source_release.tbz2 public_release/kernel_src.tbz2
+tar -xvf public_release/kernel_src.tbz2
 cd kernel/kernel-4.4
 zcat /proc/config.gz > .config
 make xconfig
